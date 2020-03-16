bc8d1479f034a801391036c160ec1dcfc8f6c9b4
HeatherCao <877795669@qq.com>
2020-03-16 23:06:30 +0800

    eureka demo

--- eureka-server-9001/src/main/java/com/study/eurekaserver9001/EurekaServer9001Application.java
+++ eureka-server-9001/src/main/java/com/study/eurekaserver9001/EurekaServer9001Application.java
@@ -2,8 +2,10 @@ package com.study.eurekaserver9001;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaServer9001Application {
 
     public static void main(String[] args) {
