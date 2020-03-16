8257e00ed82b36d555b2b2e7c7f7ddb2dc7791c7
HeatherCao <877795669@qq.com>
2020-03-16 23:02:37 +0800

    eureka demo

---
+++ eureka-server-9001/src/main/java/com/study/eurekaserver9001/EurekaServer9001Application.java
@@ -0,0 +1,13 @@
+package com.study.eurekaserver9001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServer9001Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer9001Application.class, args);
+    }
+
+}
